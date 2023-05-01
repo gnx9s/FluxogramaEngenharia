e34bf100214975c962ac9fafaf94445079c8b414
gnx9s <gabriel.rodriguesm0rg@outlook.com>
2023-04-30 21:33:06 -0300

    atividade feita com sucesso nessa paradinha tadoido

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("-- Fluxograma de Engenharia --");
+
+Console.WriteLine("O objeto se move? Responda com S/N");
+bool resposta1 = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+Console.WriteLine("Deveria se mover?");
+bool resposta2 = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+string mensagem;
+
+if (resposta1)
+{
+    if (resposta2)
+    {
+       Console.ForegroundColor = ConsoleColor.Green;
+       mensagem = "Ótimo, está funcionando perfeitamente!";
+    }
+
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        mensagem = "Use a Silver Tape.";
+    }
+}
+
+else
+{
+    if (resposta2)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        mensagem = "Use o WD-40 de preferência!";
+    }
+
+    else
+    {
+      Console.ForegroundColor = ConsoleColor.Green;
+      mensagem = "Ótimo, está funcionando perfeitamente!";
+    }
+}
+
+Console.WriteLine("");
+Console.WriteLine(mensagem);
+Console.WriteLine("");
+Console.ResetColor();
